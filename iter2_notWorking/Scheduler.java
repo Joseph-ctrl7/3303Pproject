247c8ad3e2653fdf5775fb14a48b2832d0fc57cb
amitorko <72279378+amitorko@users.noreply.github.com>
2021-02-26 06:00:02 +0600

    Create Scheduler.java

---
+++ iter2_notWorking/Scheduler.java
@@ -0,0 +1,22 @@
+package iter2;
+/**
+ *
+ */
+
+
+
+
+import java.sql.Timestamp;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Random;
+
+public class Scheduler  {
+
+ 
+
+    }
+    public Elevator getBestElevator(SchedulerRequest request) {
+    	if (elevator.getRequestDirection().equals(request.getRequestDirection())) {
+    }
+}
